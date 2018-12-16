5d17f8e4bc7d3c728798b59012711a41ac8000c7
mwafak <mwafaqarbash3@gmail.com>
2018-12-16 20:03:12 +0100

    js modified2

--- CountDownClock/js/main.js
+++ CountDownClock/js/main.js
@@ -1,16 +1,17 @@
 function countdown() {
   var now = new Date();
-  var eventDate = new Date(2016, 11, 25);
+  var eventDate = new Date(2018, 12, 22);
+
   var currentTime = now.getTime();
   var eventTime = eventDate.getTime();
 
   var remTime = eventTime - currentTime;
+  console.log(remTime);
 
   var s = Math.floor(remTime / 1000);
   var m = Math.floor(s / 60);
   var h = Math.floor(m / 60);
   var d = Math.floor(h / 24);
-
   h %= 24;
   m %= 60;
   s %= 60;
@@ -19,10 +20,10 @@ function countdown() {
   s = s < 10 ? "0" + s : s;
 
   document.getElementById("days").textContent = d;
-  document.getElementById("days").innerText = d;
   document.getElementById("hours").textContent = h;
   document.getElementById("minutes").textContent = m;
   document.getElementById("seconds").textContent = s;
 
   setTimeout(countdown, 1000);
 }
+countdown();

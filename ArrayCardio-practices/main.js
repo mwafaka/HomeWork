c0a3398087b83898d903e7cff844b89535222750
mwafak <mwafaqarbash3@gmail.com>
2018-12-27 21:23:30 +0100

    every thing be true added

--- ArrayCardio-practices/main.js
+++ ArrayCardio-practices/main.js
@@ -141,3 +141,19 @@ function binaryAgent(str) {
 binaryAgent(
   "01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"
 );
+
+///////////////////Everything Be True/////////////
+
+function truthCheck(collection, pre) {
+  // Is everyone being true?
+  return collection.every(x => x[pre]);
+}
+truthCheck(
+  [
+    { user: "Tinky-Winky", sex: "male" },
+    { user: "Dipsy", sex: "male" },
+    { user: "Laa-Laa", sex: "female" },
+    { user: "Po", sex: "female" }
+  ],
+  "sex"
+);

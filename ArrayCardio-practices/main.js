6174eb100a426b6a8197450eb4ed7071472ed061
mwafak <mwafaqarbash3@gmail.com>
2019-01-02 23:09:10 +0100

    Telephone numer validator added

--- ArrayCardio-practices/main.js
+++ ArrayCardio-practices/main.js
@@ -311,3 +311,9 @@ function convertToRoman(num) {
 }
 
 convertToRoman(36);
+///////// Telephone Number Validator/////////////
+function telephoneCheck(str) {
+  var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+  return regex.test(str);
+}
+telephoneCheck("555-555-5555");
